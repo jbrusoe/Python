0a69a829332658fe683cfd380f12a098ae80773b
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-07 21:16:09 -0400

    Added fill color demo

---
+++ Turtle/FillColor/Turtle-FillColor.py
@@ -0,0 +1,21 @@
+#Python Turtle fill color demo
+#Written by: Jeff Brusoe
+
+import turtle
+
+turtle.showturtle()
+
+turtle.penup()
+turtle.goto(-0,-200)
+turtle.pendown()
+
+turtle.fillcolor("cyan")
+turtle.begin_fill()
+
+turtle.circle(200)
+
+turtle.end_fill()
+
+
+
+

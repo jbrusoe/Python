2d0d8d71cfdcdeed37c78a047efaa0bf902faa12
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-07 21:08:19 -0400

    Added file of turtle drawing two traiangles.

---
+++ Turtle/TwoTriangles/Turtle-TwoTriangles.py
@@ -0,0 +1,31 @@
+#Turtle-TwoTriangles.py
+#Written by: Jeff Brusoe
+#
+#From Problem 1-15 of Liang Intro to Python textbook.
+
+import turtle
+
+turtle.showturtle()
+
+#Draw side 1
+turtle.setheading(300)
+turtle.forward(100)
+
+#Draw side 2
+turtle.setheading(180)
+turtle.forward(100)
+
+#Draw side 3
+turtle.setheading(60)
+turtle.forward(100)
+
+#Draw side 4
+turtle.forward(100)
+
+#Draw side 5
+turtle.setheading(180)
+turtle.forward(100)
+
+#Draw side 6
+turtle.setheading(300)
+turtle.forward(100)

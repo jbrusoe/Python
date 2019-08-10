a499bb369e28f9d711ccdeab6024fa5478002c2f
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-10 19:22:26 -0400

    Update SlopeOfALine-Ver4.py

--- SlopeOfALine-Ver4/SlopeOfALine-Ver4.py
+++ SlopeOfALine-Ver4/SlopeOfALine-Ver4.py
@@ -1,18 +1,29 @@
 #Slope of a line - Version 4
 
 #Version 4 adds code to check for valid user input with a function unlike version 3.
+#This does not use the built in isdigit() function. That will be done in version 5.
 
 #Written by: Jeff Brusoe
 #Last Updated: August 10, 2019
 
-#First Point
-while True:
+def isValidInput(UserInput):
+
     try:
-        x1 = float(input("Enter the first x coordinate: "))
+        float(UserInput)
     except:
-        print("Plesae enter a valid number for the first point")
+        print("Please enter a number.")
+        return False
     else:
-        break
+        return True
+
+#First Point
+
+UserInput = ""
+while not isValidInput(UserInput):
+        UserInput = input("Enter the first x coordinate: ")
+
+x1 = float(UserInput)
+UserInput = ""
 
 while True:
     try:

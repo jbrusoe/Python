6d685ca6283a354743926534106971ec01ed6b83
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-10 19:04:22 -0400

    Update SlopeOfALine-Ver3.py

--- SlopeOfALine-Ver3/SlopeOfALine-Ver3.py
+++ SlopeOfALine-Ver3/SlopeOfALine-Ver3.py
@@ -1,6 +1,6 @@
 #Slope of a line - Version 3
 
-#Version 2 adds code to check for valid user input (acutally a number not a string or just hitting the enter key)
+#Version 3 adds code to check for valid user input (acutally a number not a string or just hitting the enter key)
 #It is not done with a function which will be done in version 4.
 
 #Written by: Jeff Brusoe
@@ -14,12 +14,33 @@ while True:
         print("Plesae enter a valid number for the first point")
     else:
         break
-    
-y1 = float(input("Enter the first y coordinate: "))
+
+while True:
+    try:
+        y1 = float(input("Enter the first y coordinate: "))
+    except:
+        print("Plesae enter a valid number for the first point")
+    else:
+        break
+
 
 #Second Point
-x2 = float(input("Enter the second x coordinate: "))
-y2 = float(input("Enter the second y coordinate: "))
+while True:
+    try:
+        x2 = float(input("Enter the second x coordinate: "))
+    except:
+        print("Plesae enter a valid number for the first point")
+    else:
+        break
+
+while True:
+    try:
+        y2 = float(input("Enter the second y coordinate: "))
+    except:
+        print("Plesae enter a valid number for the first point")
+    else:
+        break
+
 
 try:
     slope = (y2-y1)/(x2-x1)

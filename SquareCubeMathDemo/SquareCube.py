3a72b5ca9758ddae91698b60e3e41ae9bcd64175
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-11 12:44:02 -0400

    Uploaded Square & Cube Demo

---
+++ SquareCubeMathDemo/SquareCube.py
@@ -0,0 +1,10 @@
+#Print squares and cubes of a number
+#Written by: Jeff Brusoe
+#Last Updated: February 11, 2019
+#Based on Liang Problem 1-4
+
+
+print("a\ta**2\ta**3")
+
+for i in range(1,21):
+    print(i,"\t",i**2,"\t",i**3)

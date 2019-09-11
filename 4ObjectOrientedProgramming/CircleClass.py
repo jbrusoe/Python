b76ac48a6b9eef1fcf048f6e70d7624776a23be5
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-09-11 12:23:09 -0400

    Create CircleClass.py

---
+++ 4ObjectOrientedProgramming/CircleClass.py
@@ -0,0 +1,25 @@
+#Circle Class Demo with Python
+#Written by: Jeff Brusoe
+#Last Updated: September 11, 2019
+
+import math
+
+class Circle:
+
+    #initializer
+    def __init__(self, radius = 5):
+        self.radius = radius
+
+    def getRadius(self):
+        print("Creating circle with radius " + str(radius))
+        return self.radius
+
+    def setRadius(self,radius):
+        #To do: Add code to verify radius is valid
+        if radius > 0:
+            print("Setting radius to " + str(radius))
+            self.radius = radius
+        else:
+            print("Radius: " + str(radius))
+            print("Radius is less than zero. Unable to change.")
+            

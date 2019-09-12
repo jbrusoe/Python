2a0fc6bb5e706c3cfd6e00bf2af0e5ddc24e6ffa
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-09-11 21:41:43 -0400

    Create CircleTest.py

---
+++ 5ObjectOrientedProgramming/02-CircleClass-RadiusPrivate/CircleTest.py
@@ -0,0 +1,17 @@
+#This filet tests out the constuctor and methods of the circle class.
+
+import Circle
+
+Circle1 = Circle.Circle(5)
+print("Circle Radius: " + str(Circle1.getRadius()))
+
+Circle1.setRadius(10)
+print("Circle New Radius: " + str(Circle1.getRadius()))
+
+Circle1.setRadius(1)
+print("Circle New Radius: " + str(Circle1.getRadius()))
+
+print("Circle Circumference: " + str(Circle1.getCircumference()))
+
+print("Circle 1 Radius - Access via non-hidden variable")
+print("Circle1 Radius: " + str(Circle1.radius))

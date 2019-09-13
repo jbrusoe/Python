939465111f47fdfb69a744a68d8bf0c2c6117808
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-09-13 10:05:09 -0400

    Update TkinterTextbox.py

--- 4Tkinter/05-TextBox/TkinterTextbox.py
+++ 4Tkinter/05-TextBox/TkinterTextbox.py
@@ -2,15 +2,9 @@
 
 import tkinter
 
-def clickOK():
-    print("Ok button has been clicked.")
-
 #Configure main form
 MainForm = tkinter.Tk()
-MainForm.geometry("300x50")
-MainForm.title("Tkinter Change Button Color")
-
-button = tkinter.Button(MainForm, text = "Click Me!", command = clickOK, fg="red",bg="yellow")
-button.pack()
+MainForm.geometry("300x300")
+MainForm.title("Tkinter Textbox")
 
 MainForm.mainloop()

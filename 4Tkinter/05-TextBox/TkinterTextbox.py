2e876d40deb648fcb74f1fc3aa91bd66c88303ee
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-10-05 12:01:47 -0400

    Update TkinterTextbox.py

--- 4Tkinter/05-TextBox/TkinterTextbox.py
+++ 4Tkinter/05-TextBox/TkinterTextbox.py
@@ -1,10 +1,17 @@
-#Tkinter - Change Button Color
+#Tkinter - Tkinter Textbox Example
 
 import tkinter
 
 #Configure main form
 MainForm = tkinter.Tk()
 MainForm.geometry("300x300")
-MainForm.title("Tkinter Textbox")
+MainForm.title("Tkinter Textbox Example")
+
+txtName = tkinter.Entry(MainForm,width=25)
+lblName = tkinter.Label(MainForm,text="", font=("Comic Sans",40))
+
+
+txtName.pack()
+lblName.pack()
 
 MainForm.mainloop()

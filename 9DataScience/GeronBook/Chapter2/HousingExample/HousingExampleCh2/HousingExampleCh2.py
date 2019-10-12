b52ab86fe6ae2a4092ebc85235ae7a57b145055d
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-10-12 16:01:53 -0400

    Update chapter 2 housing files

--- 9DataScience/GeronBook/Chapter2/HousingExample/HousingExampleCh2/HousingExampleCh2.py
+++ 9DataScience/GeronBook/Chapter2/HousingExample/HousingExampleCh2/HousingExampleCh2.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import os
 
+HOUSING_PATH = "C:\\Users\\jbrus\\Documents\\GitHub\\Python\\9DataScience\\GeronBook\\Chapter2\\"
+
+def load_housing_data(housing_path = HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+housing = load_housing_data()
+print(housing.head())

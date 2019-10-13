13252014920c02f3e21480773ff3b2198a1a53d7
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-10-12 21:26:51 -0400

    Added Geron Chapter 2 files

--- 9DataScience/GeronBook/Chapter2/HousingExampleCh2-ExaminingData/HousingExampleCh2.py
+++ 9DataScience/GeronBook/Chapter2/HousingExampleCh2-ExaminingData/HousingExampleCh2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 
 HOUSING_PATH = "C:\\Users\\jbrus\\Documents\\GitHub\\Python\\9DataScience\\GeronBook\\Chapter2\\"
@@ -9,3 +10,13 @@ def load_housing_data(housing_path = HOUSING_PATH):
 
 housing = load_housing_data()
 print(housing.head())
+print("\nHousing Info")
+print(housing.info())
+print("\nOcean Proximity Values")
+print(housing["ocean_proximity"].value_counts())
+print("\nDescribe Method Output")
+print(housing.describe())
+
+#Plot data
+housing.hist(bins=50, figsize=(20,15))
+plt.show()

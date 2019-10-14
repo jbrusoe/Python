c868486c3ff406f3300632555e8a9ad420dfc978
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-10-14 15:44:51 -0400

    Modified Geron Ch2 files

--- 9DataScience/GeronBook/Chapter2/02-HousingExampleCh2-CreateTestSet/HousingExampleCh2.py
+++ 9DataScience/GeronBook/Chapter2/02-HousingExampleCh2-CreateTestSet/HousingExampleCh2.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+
 import os
 
 HOUSING_PATH = "C:\\Users\\jbrus\\Documents\\GitHub\\Python\\9DataScience\\GeronBook\\Chapter2\\"
@@ -8,15 +13,23 @@ def load_housing_data(housing_path = HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
+#Main program
 housing = load_housing_data()
-print(housing.head())
-print("\nHousing Info")
-print(housing.info())
-print("\nOcean Proximity Values")
-print(housing["ocean_proximity"].value_counts())
-print("\nDescribe Method Output")
-print(housing.describe())
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
-#Plot data
-housing.hist(bins=50, figsize=(20,15))
-plt.show()
+print("\nTraining Set")
+print(train_set)
+
+print("\nTesting Set")
+print(test_set)
+
+print("\n\nCreating Stratified Data")
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+print( housing["income_cat"].value_counts() / len(housing))

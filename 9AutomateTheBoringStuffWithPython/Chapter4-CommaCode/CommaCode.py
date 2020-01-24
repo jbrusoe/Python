b83b00c05c60ea7a25de32fd5adbc4b5255c3309
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-01-24 12:26:37 -0500

    Add chapter 4 comma code files

---
+++ 9AutomateTheBoringStuffWithPython/Chapter4-CommaCode/CommaCode.py
@@ -0,0 +1,16 @@
+#CommaCode.py
+#Written by: Jeff Brusoe
+#Last Updated: Jaunary 24, 2020
+#
+#Purpose:   Convert a list into a comma separated string with the word "and"
+#           used before the last word. This is from the end of chapter 4 of
+#           "Automate the Boring Stuff with Python".
+
+def ListToString(InputList):
+    print(InputList)
+    OutputString = None
+
+    return OutputString
+
+ListToString(['apples', 'bananas', 'tofu', 'cats'])
+    

98c0ab9806245dd70901001c4431597ead099110
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-02-07 18:37:53 -0500

    Updating variable scope demo files

--- 3MiscPrograms/VariableScope/VariableScopeDemo.py
+++ 3MiscPrograms/VariableScope/VariableScopeDemo.py
@@ -1,14 +1,21 @@
-AnotherNumber = 5
+#Python - Variable Scope Demo
+#Written By: Jeff Brusoe
+#Last Updated: February 7, 2020
 
-def VariableDemo(SomeNumber):
-    FunctionNumber = 20
-    
-    print("Function Input: " + str(SomeNumber))
-    print("Another Number: " + str(AnotherNumber))
-    print("In function - Function Number: " + str(FunctionNumber))
+GlobalVariable1 = "Global definied outside of function"
 
-SomeNumber = 8
-VariableDemo(SomeNumber)
-print("SomeNumber: " + str(SomeNumber))
+def ScopeDemo():
+    print("Inside the function")
+    global GlobalVariable2
+    GlobalVariable2 = "Global defined inside of function"
+    FunctionLocalVariable = "Local variable in function"
+    print(GlobalVariable1)
+    print(FunctionLocalVariable)
+    print("Leaving function\n")
 
-#print("Outside of Function - Function Number: " + str(FunctionNumber))
+ScopeDemo()
+print("Outside the function:",GlobalVariable1)
+print("Outside the function:",GlobalVariable2)
+
+print("Attempting to print local variable from function:")
+print(FunctionLocalVariable) # Out of scope, so this gives an error

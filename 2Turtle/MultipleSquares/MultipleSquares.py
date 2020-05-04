6a46267de8361a9c98f5a3f957ffa3b36c47bcf1
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-05-03 22:47:44 -0400

    Upload multiple squares turtle example

---
+++ 2Turtle/MultipleSquares/MultipleSquares.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtle.showturtle()
+turtle.width(5)
+
+SideLength = 250
+NumberOfSquares = 5
+
+for i in range(0,NumberOfSquares):
+    turtle.setheading(i*360/NumberOfSquares)
+
+    turtle.forward(SideLength/2)
+
+    for j in range(0,3):
+        turtle.right(90)
+        turtle.forward(SideLength)
+    
+    turtle.right(90)
+    turtle.forward(SideLength/2)
+
+turtle.hideturtle()
